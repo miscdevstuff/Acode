71d57fce20e8532ebcde1b52547df45331c2d3e9
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2025-06-29 22:58:15 +0530

    feat: add listDirectory tool

--- src/pages/aiAssistant/tools/index.js
+++ src/pages/aiAssistant/tools/index.js
@@ -1,8 +1,10 @@
 import { fetchTool } from "./fetch";
+import { listDirectory } from "./listDirectory";
 import { readFile } from "./readFile";
 
 // Export all tools as a single object
 export const allTools = {
 	readFile,
 	fetchTool,
+	listDirectory,
 };
