0869ed388856551b7f4e1390e1d062b070f83c7a
Sohil876 <sohil876@gmail.com>
2025-08-27 19:40:19 +0530

    Change provider to com.foxdebug.acodex.provider

--- src/plugins/system/utils/changeProvider.js
+++ src/plugins/system/utils/changeProvider.js
@@ -14,12 +14,23 @@ module.exports = {
 
     try {
       const fileData = fs.readFileSync(configXML, "utf8");
+      const widgetMatch = /<widget\s+id=['"]([^'"]+)['"]/.exec(fileData);
+      if (!widgetMatch) {
+          throw new Error("Could not find <widget id> in config.xml");
+      }
       const manifest = fs.readFileSync(androidManifest, "utf8");
-      const ID = reset ? "com.foxdebug" : /widget id="([0-9a-zA-Z\.\-_]*)"/.exec(fileData)[1];
+      const ID = widgetMatch[1];  // always use the real id
+      //const ID = reset ? "com.foxdebug" : /widget id="([0-9a-zA-Z\.\-_]*)"/.exec(fileData)[1];
+      // const newFileData = manifest.replace(
+//         /(android:authorities=")([0-9a-zA-Z\.\-_]*)(")/,
+//         `$1${reset ? "com.foxdebug" : ID}.provider$3`
+//       );
       const newFileData = manifest.replace(
-        /(android:authorities=")([0-9a-zA-Z\.\-_]*)(")/,
-        `$1${reset ? "com.foxdebug" : ID}.provider$3`
+        // Match the FileProvider provider’s authorities attribute only
+        /(<provider[^>]*android:name="androidx\.core\.content\.FileProvider"[^>]*android:authorities=")[^"]+(")/,
+        `$1${ID}.provider$2`
       );
+
       fs.writeFileSync(androidManifest, newFileData);
 
       const msg = "==== Changed provider to " + ID + ".provider ====";
